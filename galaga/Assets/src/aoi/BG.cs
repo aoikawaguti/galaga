999a71bae9d5ccd21b61c286983dea214b7c5d0e
aoikawaguti <yda.1a11.2170003@gmail.com>
2017-11-14 11:17:49 +0900

    前回コミットの修正

--- galaga/Assets/src/aoi/BG.cs
+++ galaga/Assets/src/aoi/BG.cs
@@ -4,31 +4,60 @@ using UnityEngine;
 
 public class BG : MonoBehaviour {
 
+    Renderer dia,dia1,dia2,dia3,dia4, dia5, dia6, dia7, dia8, dia9, dia10, dia11, dia12, dia13, dia14;
+
+    int[] xaxis = new int[] { -7, -6, -5, -4, -3, -2, -1, 0, 1, 2, 3, 4, 5, 6, 7 };
+    int[] yaxis = new int[] { -7, -6, -5, -4, -3, -2, -1, 0, 1, 2, 3, 4, 5, 6, 7 };
+    public static int cnt;
+
     // Use this for initialization
     void Start () {
-        
-	}
-
-    // Update is called once per frame
-    void Update()
-    {
-
+       
         GameObject diamond = GameObject.Find("Circle");
 
-        GameObject star = GameObject.Find("Circle 1");
+        GameObject star = GameObject.Find("Circle 1"); 
 
-        GameObject obj = Instantiate(diamond, new Vector3(Random.Range(-7.0f, 7.0f), 7.0f, 0.0f), Quaternion.identity) as GameObject;
+        GameObject obj = Instantiate(diamond, new Vector3(xaxis[2], yaxis[5], 0.0f), Quaternion.identity) as GameObject;
+        
+        GameObject obj1 = Instantiate(star, new Vector3(xaxis[4], yaxis[6], 0.0f), Quaternion.identity) as GameObject;
 
-        obj.GetComponent<Renderer>().material.color = new Color(Random.value, Random.value, Random.value, 1.0f);
+        GameObject obj2 = Instantiate(diamond, new Vector3(xaxis[3], yaxis[4], 0.0f), Quaternion.identity) as GameObject;
 
-        GameObject obj1 = Instantiate(star, new Vector3(Random.Range(-7.0f, 7.0f), 7.0f, 0.0f), Quaternion.identity) as GameObject;
+        GameObject obj3 = Instantiate(star, new Vector3(xaxis[13], yaxis[5], 0.0f), Quaternion.identity) as GameObject;
 
-        obj1.GetComponent<Renderer>().material.color = new Color(Random.value, Random.value, Random.value, 1.0f);
+        GameObject obj4 = Instantiate(diamond, new Vector3(xaxis[8], yaxis[14], 0.0f), Quaternion.identity) as GameObject;
 
-        Destroy(obj, 2);
+        GameObject obj5 = Instantiate(star, new Vector3(xaxis[12], yaxis[7], 0.0f), Quaternion.identity) as GameObject;
 
-        Destroy(obj1, 2);
+        GameObject obj6 = Instantiate(diamond, new Vector3(xaxis[6], yaxis[13], 0.0f), Quaternion.identity) as GameObject;
+
+        GameObject obj7 = Instantiate(star, new Vector3(xaxis[11], yaxis[12], 0.0f), Quaternion.identity) as GameObject;
+
+        GameObject obj8 = Instantiate(diamond, new Vector3(xaxis[3], yaxis[4], 0.0f), Quaternion.identity) as GameObject;
+
+        GameObject obj9 = Instantiate(star, new Vector3(xaxis[6], yaxis[4], 0.0f), Quaternion.identity) as GameObject;
+
+        GameObject obj10 = Instantiate(diamond, new Vector3(xaxis[5], yaxis[5], 0.0f), Quaternion.identity) as GameObject;
+
+        GameObject obj11 = Instantiate(star, new Vector3(xaxis[4], yaxis[11], 0.0f), Quaternion.identity) as GameObject;
+
+        
 
     }
-    
+
+    // Update is called once per frame
+    void Update () {
+
+        if (cnt >= 10 && cnt <= 20)
+        {
+             
+            Debug.Log("cnt:" + cnt);
+
+            if (cnt == 20)
+            {
+                cnt = 0;
+            }
+        }
+
+    }
 }
--- galaga/Assets/src/aoi/Star1.cs
+++ galaga/Assets/src/aoi/Star1.cs
@@ -11,12 +11,17 @@ public class Star1 : MonoBehaviour
     void Start()
     {
         
+
     }
 
     // Update is called once per frame
     void Update()
     {
         this.transform.position -= new Vector3(0, 0.2f, 0);
+        if (this.transform.position.y <= -5)
+        {
+            this.transform.position = new Vector3(this.transform.position.x, 10, 0);
+        }
     }
 }
 
