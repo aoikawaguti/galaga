5d2c1c888c1231fbd473160d144c395615c2c5c5
Kaigakajitani <kajitani1998@gamil.com>
2017-11-24 09:07:06 +0900

    更新　11/25

--- galaga/Assets/src/taisei/player.cs
+++ galaga/Assets/src/taisei/player.cs
@@ -3,13 +3,31 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Player : MonoBehaviour {
-	// Use this for initialization
-	//GameObject Player;
+	public GameObject bulletPrefab; // 弾のオブジェクト
+	public GameObject Square;
+	public static int count;
+	public static int dualflg;
+	int frame;
+
 	void Start () {
-		//Player = GameObject.Find ("Player");
+		dualflg = 0;	//0　操作可能　１　操作不能　2　dual状態
+		frame = 0;
 	}
-	// Update is called once per frame
 	void Update () {
+		　　
+		if (dualflg == 1) {		//捕まった状態
+			transform.Rotate (new Vector2(180,0));
+			transform.position = Square.transform.position;
+		}
+		if(dualflg == 0){		//捕まってない状態
+		frame ++;
+		if(Input.GetKey(KeyCode.Space) && count <= 1 && frame > 10)	//弾数３以上は出ない
+		{
+			count++;
+			Instantiate(bulletPrefab,new Vector2(transform.position.x-0.021f,transform.position.y),Quaternion.identity); // プレイヤーの位置に弾を生成します
+			frame = 0;
+		}
+
 		if (Input.GetKey (KeyCode.RightArrow)) {
 			transform.position += new Vector3 (0.1f, 0);
 		}
@@ -17,10 +35,11 @@ public class Player : MonoBehaviour {
 			transform.position += new Vector3 (-0.1f, 0);
 		}
 		if (transform.position.x < -6.25f) {
-			transform.position = new Vector3(-6.25f,transform.position.y,0);	//移動制限（左）
+			transform.position = new Vector3 (-6.25f, transform.position.y, 0);	//移動制限（左）
 		}
 		if (transform.position.x > 6.25f) {
-			transform.position = new Vector3(6.25f,transform.position.y,0);		//移動制限（右）
+			transform.position = new Vector3 (6.25f, transform.position.y, 0);		//移動制限（右）
+			}
 		}
 	}
 }
