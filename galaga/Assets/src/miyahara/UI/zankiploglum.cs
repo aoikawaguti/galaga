05062ac36420e34daf6ec826834a0b55af10b9c7
yousuke <syobonn.yda@gmail.com>
2017-12-22 11:47:36 +0900

    残機の複製

--- galaga/Assets/src/miyahara/UI/zankiploglum.cs
+++ galaga/Assets/src/miyahara/UI/zankiploglum.cs
@@ -3,16 +3,23 @@ using System.Collections.Generic;
 using UnityEngine;
 
 
-public class zankiploglum : MonoBehaviour {
-    public static int zanki_UI_6;
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-        Player _zanki_UI_6;
-    
-	}
+public class zankiploglum: MonoBehaviour
+{
+    public GameObject original;
+    public GameObject copied;
+    public GameObject copied2;
+    // Use this for initialization
+    void Start()
+    {
+        copied = Instantiate(original) as GameObject;    //左側の残機
+        copied2 = Instantiate(original) as GameObject;　//右側の残機
+        copied2.transform.position = new Vector3(6.2f,-2.12f,0);
+        //copied.transform.Translate(2, 0, 0);
+    }
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
 }
+    
