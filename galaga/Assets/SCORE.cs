bf2909610292148acdbf4e6a93b27b4fc6f1a0a8
yousuke <syobonn.yda@gmail.com>
2018-02-16 18:08:27 +0900

    タイトル　リザルトの文字やらを変えました

--- galaga/Assets/SCORE.cs
+++ galaga/Assets/SCORE.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 using UnityEngine.UI;
 public class SCORE : MonoBehaviour {
     int ScoreData = 0;
+    //int ScoreaData = 0;
 	// Use this for initialization
 	void Start () {
 		
@@ -12,10 +13,11 @@ public class SCORE : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
         GetComponent<Text>().text = "" + ScoreData.ToString("00");
-        
-	}
+        //GetComponent<Text>().text = "" + ScoreaData.ToString("00");
+    }
     void ScoreUp()
     {
         ScoreData += 100;
+        //ScoreaData += 100;
     }
 }
--- galaga/Assets/src/miyahara/SCORE.cs
+++ galaga/Assets/src/miyahara/SCORE.cs
@@ -3,18 +3,18 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 //public class SCORE : MonoBehaviour {
-    //int ScoreData = 0;
-	// Use this for initialization
+   //int ScoreData = 0;
+	 //Use this for initialization
 	//void Start () {
 		
 	//}
 	
-	// Update is called once per frame
+	 //Update is called once per frame
 	//void Update () {
         //GetComponent<Text>().text = "SCORE:" + ScoreData.ToString("0");
         
 	//}
-   // void ScoreUp()
+    //void ScoreUp()
     //{
         //ScoreData += 100;
     //}
